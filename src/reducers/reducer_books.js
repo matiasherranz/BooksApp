a2863184b4002fb77d544ada57573ca31b57b2fb
Matias Herranz <matias.herranz@santexgroup.com>
2016-12-07 12:36:30 -0300

    Initial commit.

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The good parts', pages: 10},
+    { title: 'Javascript: The weird parts', pages: 100},
+    { title: 'Javascript: The bad parts', pages: 20},
+    { title: 'Javascript: The other parts', pages: 1234}
+  ]
+}
